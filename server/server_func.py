49f76f874109c4abea7ddf75ec0e785a2580d243
subinacls <admin@hacker-island.com>
2014-06-18 17:31:30 -0500

    added some try except conditionals and other modifications to the code base - added in threadedudpserver and some other requirements / modifications in other files to accomidate

--- server/server_func.py
+++ server/server_func.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
 #!/usr/bin/env python
-import verinfo
-from bcolors import bcolors
+
 import os
 import sys
 import time
+import verinfo
+from bcolors import bcolors
 
 va = __appname__
 vv = __version__
@@ -23,20 +24,17 @@ class tcp_servers():
 
 	def tserver(self,serverport):
 		try:
-			from threadedtcpserver import ThreadedTCPRequestHandler 
-			from threadedtcpserver import ThreadedTCPServer
-			from threadedtcpserver import tcpserver
+			from threadedtcpserver import ThreadedTCPRequestHandler, ThreadedTCPServer, tcpserver
 			tcpserver().mytcpserver(int(serverport))
-			while 1: 
-				try:
-					time.sleep(0) 
-				except KeyboardInterrupt: 
-					pid = os.getpid()
-					os.popen("iptables -t nat -F")
-					print bo+"\t[!] User exited - flushed IPTABLES and killed server pid "+str(pid)+" ...\n" + be
-					killer = "kill -9 "+str(pid)
-					os.popen(killer) 
-					sys.exit(0) 
+			try:
+				time.sleep(0)
+			except KeyboardInterrupt:
+				pid = os.getpid()
+				os.popen("iptables -t nat -F")
+				print bo+"\t[!] User exited - flushed IPTABLES and killed server pid "+str(pid)+" ...\n" + be
+				killer = "kill -9 "+str(pid)
+				os.popen(killer)
+				sys.exit(0)
 		except Exception as e: 
 			print "\t[?] " +va+" v."+vv+" - CRASHED during import of tcp server: " + bf + str(e) + "\n" +be 
 			sys.exit(0)
@@ -48,20 +46,17 @@ class udp_servers:
 
 	def userver(self, serverport):
 		try:
-			from threadedudpserver import ThreadedUDPRequestHandler
-			from threadedudpserver import ThreadedUDPServer
-			from threadedudpserver import udpserver
+			from threadedudpserver import ThreadedUDPRequestHandler, ThreadedUDPServer, udpserver
 			udpserver(int(serverport)).myudpserver(int(serverport))
-			while 1: 
-				try: 
-					time.sleep(0)
-				except KeyboardInterrupt:
-					pid = os.getpid()
-					os.popen("iptables -t nat -F") 
-					print bo+"\t[!] User exited - flushed IPTABLES and killed server pid "+str(pid)+" ...\n" + be
-					killer = "kill -9 "+str(pid)
-					os.popen(killer)
-					sys.exit(0) 
+			try:
+				time.sleep(0)
+			except KeyboardInterrupt:
+				pid = os.getpid()
+				os.popen("iptables -t nat -F")
+				print bo+"\t[!] User exited - flushed IPTABLES and killed server pid "+str(pid)+" ...\n" + be
+				killer = "kill -9 "+str(pid)
+				os.popen(killer)
+				sys.exit(0)
 		except Exception as e: 
 			print "\t[?] " +va+" v."+vv+" - CRASHED during import of udp servers: " + bf + str(e) + "\n" +be 
 			sys.exit(0)
@@ -73,24 +68,19 @@ class both_servers:
 
 	def bserver(self, serverport):
 		try:
-			from threadedbothserver import ThreadedTCPRequestHandler
-			from threadedbothserver import ThreadedTCPServer
-			from threadedbothserver import btcpserver
-			from threadedbothserver import ThreadedUDPRequestHandler
-			from threadedbothserver import ThreadedUDPServer
-			from threadedbothserver import budpserver
-			budpserver(int(serverport)).myudpserver(int(serverport)) 
-			btcpserver(int(serverport)).mytcpserver(int(serverport))
-			while 1:
-				try:
-					time.sleep(0)
-				except KeyboardInterrupt:
-					pid = os.getpid()
-					os.popen("iptables -t nat -F")
-					print bo+"\t[!] User initialized exit - flushed IPTABLES and killed server pid "+str(pid)+" ...\n" + be
-					killer = "kill -9 "+str(pid)
-					os.popen(killer)
-					sys.exit(0) 
+			from threadedudpserver import ThreadedUDPRequestHandler, ThreadedUDPServer, udpserver
+			udpserver(int(serverport)).myudpserver(int(serverport))
+			from threadedtcpserver import ThreadedTCPRequestHandler, ThreadedTCPServer, tcpserver
+			tcpserver().mytcpserver(int(serverport))
+			try:
+				time.sleep(0)
+			except KeyboardInterrupt:
+				pid = os.getpid()
+				os.popen("iptables -t nat -F")
+				print bo+"\t[!] User initialized exit - flushed IPTABLES and killed server pid "+str(pid)+" ...\n" + be
+				killer = "kill -9 "+str(pid)
+				os.popen(killer)
+				sys.exit(0)
 		except Exception as e:
 			print "\t[?] " +va+" v."+vv+" - CRASHED during import of both servers: " + bf + str(e) + "\n" +be 
 			sys.exit(0)
